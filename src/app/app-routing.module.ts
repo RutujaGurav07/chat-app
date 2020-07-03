9f700e6f383752c781e7dce53285353f57fa094f
Rutuja Gurav <rutujagurav72@gmail.com>
2020-07-03 15:38:02 +0530

    upadating login component and applying router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { LoginComponent } from './login/login.component';
+import { ChatComponent } from './chat/chat.component';
+import { ChatGuard } from './chat/chat.guard';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path:'login',component:LoginComponent},
+  {path:'chat',component:ChatComponent,canActivate: [ChatGuard]},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { Router } from '@angular/router';
+import { AuthService } from '../core/auth.service';
 
 import { Login } from "./login";
 
@@ -12,29 +13,39 @@ import { Login } from "./login";
 export class LoginComponent implements OnInit {
 
   loginForm: FormGroup;
-  login= new Login();
+  login = new Login();
   submitted = false;
+  error: string;
 
 
-  constructor(private fb:FormBuilder,private router:Router) { }
+  constructor(private fb: FormBuilder, readonly router: Router, readonly auth: AuthService) { }
 
   ngOnInit(): void {
-    this.loginForm =this.fb.group({
-      userName:['',[Validators.required, Validators.minLength(3)]],
-      password:['',[Validators.required]],
+    this.loginForm = this.fb.group({
+      userName: ['', [Validators.required, Validators.minLength(3)]],
+      password: ['', [Validators.required]],
     })
   }
 
-  onSubmit() {
-    this.submitted = true;
+  // onSubmit() {
+  //   this.submitted = true;
 
-    // stop here if form is invalid
-    if (this.loginForm.invalid) {
-      return;
-    }
+  //   // stop here if form is invalid
+  //   if (this.loginForm.invalid) {
+  //     return;
+  //   }
 
-    // display form values on success
-    alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
+  //   // display form values on success
+  //   alert('SUCCESS!! :-)\n\n' + JSON.stringify(this.loginForm.value, null, 4));
+  // }
+  Log_in (userId : string) {
+  this.auth
+    .login(userId )
+    .then(
+      () => this.router.navigateByUrl('/chat'),
+      err => (this.error = err)
+    );
   }
+  
  
 }
