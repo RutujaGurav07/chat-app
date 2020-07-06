81e0fe9ecd9aae9811f3b69d1562f02b8ec7b5b1
Rutuja Gurav <rutujagurav72@gmail.com>
2020-07-06 13:47:02 +0530

    solving issue regarding appId

--- src/app/core/auth.service.ts
+++ src/app/core/auth.service.ts
@@ -1,9 +1,27 @@
 import { Injectable } from '@angular/core';
+import {MatSnackBar} from '@angular/material/snack-bar';
+import { CometChat } from '@cometchat-pro/chat';
+
+import { environment } from '../../environments/environment';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private snackBar:MatSnackBar) { 
+    this.init(environment.appId);
+  }
+  init(appId: string) {
+    CometChat.init(appId).then(
+         msg => console.log('Initialized succesfull: ', msg),
+         err => {
+           console.log('App init failed', err);
+           this.snackBar.open(
+             'App initialization failed. Please refresh the page.'
+           );
+         }
+       );
+   }
 }
