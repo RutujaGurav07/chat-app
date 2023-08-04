54ba13e35d950e0f03adce624c997f04efea06dc
Rutuja Gurav <55711030+RutujaGurav07@users.noreply.github.com>
2023-08-04 16:47:19 +0530

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
-  console.log("trying...")
+  console.log("trying... ")
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
